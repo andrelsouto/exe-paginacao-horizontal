4babf49bb2605b4c5251994d5c95c4ca39a7d406
André Luiz <a.luiz.souto@gmail.com>
2019-09-23 13:19:35 -0300

    Add files via upload

--- src/app/shared/animations/fly-left-rigth.ts
+++ src/app/shared/animations/fly-left-rigth.ts
@@ -2,9 +2,14 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 
 export const flyLeftToRigthAnimation =
     trigger('flyLeftRigth', [
-        state('in', style({transform: 'translateX(0)'})),
-        transition(':enter', [
+        state('next', style({transform: 'translateX(0)'})),
+        state('previous', style({transform: 'translateX(0)'})),
+        transition('previous => *', [
         style({transform: 'translateX(100%)'}),
+        animate(150)
+        ]),
+        transition('next => *', [
+        style({transform: 'translateX(-100%)'}),
         animate(150)
         ])
     ]);
